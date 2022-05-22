6b2be2ff6125f59707a1b2eb65dc4bfc4239a5c7
Lin, Ying-Chin <mirror770109@gmail.com>
2022-05-22 23:36:04 +0800

    Add json/convert.SnakeCaseHanlder

---
+++ convert/transport.go
@@ -0,0 +1,33 @@
+package convert
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"gitlab.com/mirror520/json/endpoint"
+)
+
+func SnakeCaseHandler(endpoint endpoint.Endpoint) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var request map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintln(w, err.Error())
+			return
+		}
+
+		response, err := endpoint(context.Background(), request)
+		if err != nil {
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			fmt.Fprintln(w, err.Error())
+			return
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	}
+}
--- main.go
+++ main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"encoding/json"
+	"log"
+	"net/http"
 	"os"
+	"time"
 
+	"gitlab.com/mirror520/json/convert"
 	"gitlab.com/mirror520/json/cors"
 )
 
 func main() {
+	svc := convert.NewService()
+	endpoint := convert.SnakeCaseEndpoint(svc)
+	handler := convert.SnakeCaseHandler(endpoint)
+
+	mux := http.NewServeMux()
+	mux.Handle("/json/convert", handler)
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Println("Listen and Server in https://127.0.0.1:8080")
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func loadCORSRulesFromFile(filename string) (*cors.Config, error) {

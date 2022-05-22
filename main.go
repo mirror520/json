7772686b2a467b23b8d4f9afe7d4eceaaf10ebb1
Lin, Ying-Chin <mirror770109@gmail.com>
2022-05-22 13:48:39 +0800

    定義 CORS 設定檔結構

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+type CORSConfig struct {
+	Version string
+	Rules   []CORSRule
+}
+
+type CORSRule struct {
+	Resource        CORSResource
+	AllowOrigins    []string
+	AllowMethods    []string
+	AllowCredential bool
+	ExposeHeaders   []string
+}
+
+type CORSResource struct {
+	Path       string
+	StartsWith bool
+	Exact      bool
+}
+
 func main() {
 	fmt.Println("Hello world")
 }
